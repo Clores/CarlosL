96c1e0e2afcbc2dd4ef374200936f69960e4c67d
Carlos Lores <lorescarlosj@gmail.com>
2018-08-06 23:20:06 -0400

    adding Alphauno Stuff

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name alphaunoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the alphaunoApp
+ */
+angular.module('alphaunoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name alphaunoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the alphaunoApp
+ */
+angular.module('alphaunoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
